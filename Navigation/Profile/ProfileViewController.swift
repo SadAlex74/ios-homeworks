2806cc13eef5cf022e157add54504531c09a56f5
SadAlex74 <admin@nukupi.ru>
2023-08-22 22:07:22 +0500

    Вторая часть задания выполнена

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -12,7 +12,7 @@ class ProfileViewController: UIViewController {
     fileprivate let data = feedPost
     
     private lazy var profileTableView: UITableView = {
-        let tableView = UITableView()
+        let tableView = UITableView(frame: .zero, style: .grouped)
         tableView.translatesAutoresizingMaskIntoConstraints = false
         return tableView
     }()
@@ -34,15 +34,24 @@ class ProfileViewController: UIViewController {
             profileTableView.topAnchor.constraint(equalTo: safeAreaGeide.topAnchor),
             profileTableView.bottomAnchor.constraint(equalTo: safeAreaGeide.bottomAnchor),
             profileTableView.leftAnchor.constraint(equalTo: safeAreaGeide.leftAnchor),
-            profileTableView.rightAnchor.constraint(equalTo: safeAreaGeide.rightAnchor)
+            profileTableView.rightAnchor.constraint(equalTo: safeAreaGeide.rightAnchor),
+            
+            
         ])
     }
     
     private func tuneTableView() {
         profileTableView.rowHeight = UITableView.automaticDimension
-        
+        profileTableView.estimatedRowHeight = 44.0
         let headerView = ProfileHeaderView()
-        profileTableView.tableHeaderView = headerView
+        
+        profileTableView.setAndLayout(headerView: headerView)
+        
+        profileTableView.tableHeaderView?.translatesAutoresizingMaskIntoConstraints = false
+        profileTableView.tableHeaderView?.autoresizesSubviews = true
+        profileTableView.dataSource = self
+        profileTableView.delegate = self
+        profileTableView.register(PostTableViewCell.self, forCellReuseIdentifier: "post")
     }
 }
 
@@ -53,9 +62,35 @@ extension ProfileViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
-        return UITableViewCell()
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: "post",
+            for: indexPath
+        ) as? PostTableViewCell else {
+            fatalError("could not dequeueReusableCell")
+        }
+        
+        cell.update(data[indexPath.row])
+        
+
+        return cell
     }
     
     
+}
+
+
+extension UITableView {
+    
+    func setAndLayout(headerView: UIView) {
+        tableHeaderView = headerView
+        headerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            headerView.widthAnchor.constraint(equalTo: widthAnchor)
+        ])
+        
+        headerView.setNeedsLayout()
+        headerView.layoutIfNeeded()
+        headerView.frame.size = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+    }
 }
