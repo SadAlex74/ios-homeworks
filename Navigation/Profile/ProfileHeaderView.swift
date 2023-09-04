b2903b99089365e61fdbd5a9f4371795215e9441
SadAlex74 <admin@nukupi.ru>
2023-09-04 23:46:03 +0500

    Done

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -30,6 +30,27 @@ class ProfileHeaderView: UIView {
     
     private var statusText = ""
     
+    private lazy var blankView: UIView = {
+        let view = UIView(frame: CGRect(x: 0, y: 0, width: 500, height: 2000))
+        view.backgroundColor = .white
+        view.layer.opacity = 0
+        return view
+    }()
+    
+    private lazy var closeImage: UIImageView = {
+        let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(systemName: "xmark.circle")
+        image.layer.opacity = 0
+        image.isUserInteractionEnabled = true
+        let tapClose = UITapGestureRecognizer(
+            target: self,
+            action: #selector(tapClose)
+            )
+        image.addGestureRecognizer(tapClose)
+         return image
+    }()
+    
     private lazy var avatarImageView: UIImageView = {
         let view = UIImageView()
         view.image = UIImage(named: "avatar")
@@ -39,8 +60,8 @@ class ProfileHeaderView: UIView {
         view.layer.borderWidth = 3
         view.layer.borderColor = UIColor.white.cgColor
         view.contentMode = .scaleAspectFit
-        
-        let tapAvatar = UIGestureRecognizer(
+        view.isUserInteractionEnabled = true
+        let tapAvatar = UITapGestureRecognizer(
             target: self,
             action: #selector(tapAvatar)
             )
@@ -97,11 +118,14 @@ class ProfileHeaderView: UIView {
     }()
     
     override func willMove(toSuperview newSuperview: UIView?) {
-        addSubview(avatarImageView)
         addSubview(fullNameLabel)
         addSubview(statusLabel)
         addSubview(setStatusButton)
         addSubview(statusTextField)
+        addSubview(blankView)
+        addSubview(avatarImageView)
+        addSubview(closeImage)
+
         setupConstraints()
         statusTextField.becomeFirstResponder()
     }
@@ -121,7 +145,47 @@ class ProfileHeaderView: UIView {
     }
     
     @objc func tapAvatar() {
-        
+        avatarZoomIn()
+    }
+    
+    private func closeImageSetup(){
+        let animator = UIViewPropertyAnimator(duration: 0.3, curve: .linear){ [self] in
+            closeImage.layer.opacity = 0.75
+        }
+       animator.startAnimation()
+    }
+    private func avatarZoomIn() {
+        let animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut) {
+            self.avatarImageView.layer.cornerRadius = 0
+        }
+        animator.addAnimations { [self] in
+            avatarImageView.layer.frame.size = CGSize(width: (self.window?.layer.frame.width)!, height: (self.window?.layer.frame.width)!)
+            avatarImageView.center.x  = (self.window?.layer.frame.width)!/2
+            avatarImageView.center.y  = (self.window?.layer.frame.height)!/2
+            blankView.layer.opacity = 0.75
+        }
+        animator.addCompletion{ [self] finishedPosition in
+            closeImageSetup()
+        }
+        animator.startAnimation()
+    }
+    
+    @objc func tapClose() {
+        avatarZoomOut()
+    }
+    
+    private func avatarZoomOut() {
+        let animator = UIViewPropertyAnimator(duration: 0.5, curve: .easeInOut) {
+            self.avatarImageView.layer.cornerRadius = 50
+        }
+        animator.addAnimations { [self] in
+            avatarImageView.layer.frame.size = CGSize(width: 100, height: 100)
+            avatarImageView.center.x  = 66
+            avatarImageView.center.y  = 66
+            blankView.layer.opacity = 0
+            closeImage.layer.opacity = 0
+        }
+        animator.startAnimation()
     }
     
     private func setupConstraints() {
@@ -148,8 +212,11 @@ class ProfileHeaderView: UIView {
             statusTextField.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 16),
             statusTextField.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
             statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 16),
-            statusTextField.heightAnchor.constraint(equalToConstant: 40)
-
+            statusTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            closeImage.topAnchor.constraint(equalTo: topAnchor, constant: 8),
+            closeImage.rightAnchor.constraint(equalTo: rightAnchor, constant: -8)
+ 
         ])
     }
 }
